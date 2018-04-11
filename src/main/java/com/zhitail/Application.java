b154b0cdf370a31a84d24138db35b7f72dc23bb8
ChenHuan <chscls@163.com>
2018-04-11 14:07:07 +0800

    1111111

--- src/main/java/com/zhitail/Application.java
+++ src/main/java/com/zhitail/Application.java
@@ -1,5 +1,37 @@
 package com.zhitail;
 
-public class Application {
+import javax.servlet.MultipartConfigElement;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@SpringBootApplication
+@ServletComponentScan
+
+public class Application {
+	
+	public static void main(String[] args) {
+			SpringApplication.run(Application.class, args);
+	
+	}
+	
+	 @Bean  
+	    public MultipartConfigElement multipartConfigElement() {  
+	        MultipartConfigFactory factory = new MultipartConfigFactory();  
+	        factory.setMaxFileSize("10000MB");  
+	        factory.setMaxRequestSize("10000MB");  
+	        return factory.createMultipartConfig();  
+	    }  
 }
--- src/main/java/com/zhitail/Test.java
+++
@@ -1,5 +0,0 @@
-package com.zhitail;
-
-public class Test {
-
-}
