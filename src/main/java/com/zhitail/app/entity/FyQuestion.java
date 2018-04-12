0b367da7b640eb6b4f9c0749b5a65dcf162b557d
ChenHuan <chscls@163.com>
2018-04-12 10:08:33 +0800

    1111111111

---
+++ src/main/java/com/zhitail/app/entity/FyQuestion.java
@@ -0,0 +1,73 @@
+package com.zhitail.app.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Transient;
+
+import com.alibaba.fastjson.JSONArray;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.zhitail.app.entity.middle.FyQuestionItem;
+
+public class FyQuestion {
+	public enum Type{
+		single,mutiply,judge
+	}//单选
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	/**
+	 * 标题
+	 */
+	private String title;
+	
+	public String getJson() {
+		return json;
+	}
+	public void setJson(String json) {
+		this.json = json;
+	}
+	/**
+	 * 标题
+	 */
+	private Long userId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	/**
+	 * 标题
+	 */
+	@JsonIgnore
+	@Lob
+	private String json;
+	@Transient
+	private List<FyQuestionItem> getItems(){
+		if(json!=null){
+			return JSONArray.parseArray(this.json,FyQuestionItem.class);
+			}else{
+				return new ArrayList<FyQuestionItem>(0);
+			}
+	}
+}
---
+++ src/main/java/com/zhitail/app/entity/middle/FyQuestionItem.java
@@ -0,0 +1,25 @@
+package com.zhitail.app.entity.middle;
+
+
+
+
+public class FyQuestionItem {
+	
+	private java.lang.String content;
+	
+	private java.lang.Boolean isSolution;
+	
+	public java.lang.Boolean getIsSolution() {
+		return isSolution;
+	}
+	public void setIsSolution(java.lang.Boolean isSolution) {
+		this.isSolution = isSolution;
+	}
+	public java.lang.String getContent() {
+		return content;
+	}
+	public void setContent(java.lang.String content) {
+		this.content = content;
+	}
+
+}
