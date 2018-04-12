f8506e7c9789c21c58683ada2d88440de1a4feba
ChenHuan <chscls@163.com>
2018-04-12 09:55:38 +0800

    1111111

---
+++ src/main/java/com/zhitail/app/entity/FyUser.java
@@ -0,0 +1,104 @@
+package com.zhitail.app.entity;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "fy_user")
+public class FyUser {
+	public enum Type{
+		admin,user
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private Type type;
+	
+	@Column(unique=true)
+	private String mobile;
+	
+	@Column(unique=true)
+	private String openid;
+	
+	@Column(unique=true)
+	private String realname;
+	
+	@Column(unique=true)
+	private String nickname;
+	
+	private String headImg;
+	
+	private Long score=0L;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getOpenid() {
+		return openid;
+	}
+
+	public void setOpenid(String openid) {
+		this.openid = openid;
+	}
+
+	public String getRealname() {
+		return realname;
+	}
+
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getHeadImg() {
+		return headImg;
+	}
+
+	public void setHeadImg(String headImg) {
+		this.headImg = headImg;
+	}
+
+	public Long getScore() {
+		return score;
+	}
+
+	public void setScore(Long score) {
+		this.score = score;
+	}
+	
+}
