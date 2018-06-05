608a77aee5cbacc9498917e6812193f401610633
chscls@163.com <chscls@163.com>
2018-06-05 22:02:38 +0800

    1111111

--- src/main/java/com/zhitail/app/config/WebConfig.java
+++ src/main/java/com/zhitail/app/config/WebConfig.java
@@ -112,15 +112,17 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	 private Connector createSslConnector() {
 	        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 	        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
-	        
-	            File keystore = new File("D:/your-name.jks");
+	        String x = env.getProperty("https.file");
+	        String y = env.getProperty("https.password");
+	        String p = env.getProperty("https.port");
+	            File keystore = new File(x);
 	            /*File truststore = new ClassPathResource("sample.jks").getFile();*/
 	            connector.setScheme("https");
 	            connector.setSecure(true);
-	            connector.setPort(443);
+	            connector.setPort(Integer.parseInt(p));
 	            protocol.setSSLEnabled(true);
 	            protocol.setKeystoreFile(keystore.getAbsolutePath());
-	            protocol.setKeystorePass("214719198680595");
+	            protocol.setKeystorePass(y);
 	           // protocol.setKeyPass(key_password);
 	            return connector;
 	      
