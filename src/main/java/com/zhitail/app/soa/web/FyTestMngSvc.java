563fc21e27111a578fc9ae0d5d6a3d18771915dd
chscls@163.com <chscls@163.com>
2018-06-09 09:38:47 +0800

    11111111111111

--- src/main/java/com/zhitail/app/soa/web/FyTestMngSvc.java
+++ src/main/java/com/zhitail/app/soa/web/FyTestMngSvc.java
@@ -1,7 +1,18 @@
 package com.zhitail.app.soa.web;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+
+import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,6 +32,12 @@ import com.zhitail.app.soa.LoginManager;
 import com.zhitail.frame.common.annotion.TokenAuth;
 import com.zhitail.frame.util.page.Pagination;
 import com.zhitail.frame.util.service.Result;
+import com.zhitail.test.entity.User;
+
+import com.zhitail.test.generator.PdfGenerator;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 @RequestMapping("/services/FyTestMngSvc")
 @RestController
 public class FyTestMngSvc {
@@ -30,6 +47,73 @@ public class FyTestMngSvc {
 	private FyTestMng testMng;
 	@Autowired
 	private FyUserMng userMng;
+	 @Resource
+	    Configuration cfg;
+	public Map<String, Object> getContent() throws IOException {
+
+		// 从数据库中获取数据， 出于演示目的， 这里数据不从数据库获取， 而是直接写死
+		
+		Map<String, Object> variables = new HashMap<String, Object>(3);
+
+		List<User> userList = new ArrayList<User>();
+
+		User tom = new User("Tom", 19, 1);
+		User amy = new User("Amy", 28, 0);
+		User leo = new User("Leo", 23, 1);
+
+		userList.add(tom);
+		userList.add(amy);
+		userList.add(leo);
+
+		variables.put("title", "用户列表");
+		variables.put("userList", userList);
+		
+		return variables;
+	}
+	public  String generate(String template)
+			throws Exception {
+
+		Template tp = cfg.getTemplate(template);
+		StringWriter stringWriter = new StringWriter();
+		BufferedWriter writer = new BufferedWriter(stringWriter);
+		tp.setEncoding("UTF-8");
+		tp.process(getContent(), writer);
+		String htmlStr = stringWriter.toString();
+		writer.flush();
+		writer.close();
+		return htmlStr;
+	}
+	@RequestMapping(value = "/ok",method=RequestMethod.GET)
+	public Result ok() {
+		
+		try {
+			String outputFile = "D:\\sample.pdf";
+			Map<String, Object> variables = new HashMap<String, Object>(3);
+
+			List<User> userList = new ArrayList<User>();
+
+			User tom = new User("Tom", 19, 1);
+			User amy = new User("Amy", 28, 0);
+			User leo = new User("Leo", 23, 1);
+
+			userList.add(tom);
+			userList.add(amy);
+			userList.add(leo);
+
+			variables.put("title", "用户列表");
+			variables.put("userList", userList);
+
+			String htmlStr = generate("fileTemplate.html");
+
+			OutputStream out = new FileOutputStream(outputFile);
+			PdfGenerator.generatePlus(htmlStr, out);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return new Result("xxxxxxxx");
+	}
 	@TokenAuth(value="token")
 	@RequestMapping(value = "/findTest",method=RequestMethod.GET)
 	public Result findQuestion(String token,Long id) {
