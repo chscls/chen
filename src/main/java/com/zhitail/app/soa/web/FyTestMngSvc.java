3808c6265e8dc02da9f2db9e4cefb6f02332e947
chscls@163.com <chscls@163.com>
2018-06-10 22:46:11 +0800

    11111111111

--- src/main/java/com/zhitail/app/soa/web/FyTestMngSvc.java
+++ src/main/java/com/zhitail/app/soa/web/FyTestMngSvc.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.StringWriter;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -13,7 +14,9 @@ import java.util.Map;
 import java.util.UUID;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -23,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.zhitail.app.entity.FyQuestion;
 import com.zhitail.app.entity.FyTest;
+import com.zhitail.app.entity.FyTestRecord;
 import com.zhitail.app.entity.FySensitive;
 import com.zhitail.app.entity.FyUser;
 import com.zhitail.app.manager.FyTestMng;
@@ -47,73 +51,7 @@ public class FyTestMngSvc {
 	private FyTestMng testMng;
 	@Autowired
 	private FyUserMng userMng;
-	 @Resource
-	    Configuration cfg;
-	public Map<String, Object> getContent() throws IOException {
-
-		// 从数据库中获取数据， 出于演示目的， 这里数据不从数据库获取， 而是直接写死
-		
-		Map<String, Object> variables = new HashMap<String, Object>(3);
-
-		List<User> userList = new ArrayList<User>();
-
-		User tom = new User("Tom", 19, 1);
-		User amy = new User("Amy", 28, 0);
-		User leo = new User("Leo", 23, 1);
-
-		userList.add(tom);
-		userList.add(amy);
-		userList.add(leo);
-
-		variables.put("title", "用户列表");
-		variables.put("userList", userList);
-		
-		return variables;
-	}
-	public  String generate(String template)
-			throws Exception {
-
-		Template tp = cfg.getTemplate(template);
-		StringWriter stringWriter = new StringWriter();
-		BufferedWriter writer = new BufferedWriter(stringWriter);
-		tp.setEncoding("UTF-8");
-		tp.process(getContent(), writer);
-		String htmlStr = stringWriter.toString();
-		writer.flush();
-		writer.close();
-		return htmlStr;
-	}
-	@RequestMapping(value = "/ok",method=RequestMethod.GET)
-	public Result ok() {
-		
-		try {
-			String outputFile = "D:\\sample.pdf";
-			Map<String, Object> variables = new HashMap<String, Object>(3);
-
-			List<User> userList = new ArrayList<User>();
-
-			User tom = new User("Tom", 19, 1);
-			User amy = new User("Amy", 28, 0);
-			User leo = new User("Leo", 23, 1);
-
-			userList.add(tom);
-			userList.add(amy);
-			userList.add(leo);
-
-			variables.put("title", "用户列表");
-			variables.put("userList", userList);
-
-			String htmlStr = generate("fileTemplate.html");
-
-			OutputStream out = new FileOutputStream(outputFile);
-			PdfGenerator.generatePlus(htmlStr, out);
-
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		
-		return new Result("xxxxxxxx");
-	}
+	
 	@TokenAuth(value="token")
 	@RequestMapping(value = "/findTest",method=RequestMethod.GET)
 	public Result findQuestion(String token,Long id) {
--- src/main/java/com/zhitail/app/soa/wx/FyTestRecordSvc.java
+++ src/main/java/com/zhitail/app/soa/wx/FyTestRecordSvc.java
@@ -1,5 +1,9 @@
 package com.zhitail.app.soa.wx;
 
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -7,11 +11,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zhitail.app.entity.FyQuestion;
@@ -28,6 +37,11 @@ import com.zhitail.app.soa.LoginManager;
 import com.zhitail.frame.common.annotion.TokenAuth;
 import com.zhitail.frame.util.page.Pagination;
 import com.zhitail.frame.util.service.Result;
+import com.zhitail.test.entity.User;
+import com.zhitail.test.generator.PdfGenerator;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 @RequestMapping("/services/FyTestRecordSvc")
 @RestController
 public class FyTestRecordSvc {
@@ -39,7 +53,45 @@ public class FyTestRecordSvc {
 	private FyTestMng testMng;
 	@Autowired
 	private FyUserMng userMng;
-	
+	 @Resource
+	 private   Configuration cfg;
+
+	public  String generate(Map<String, Object> content,String template)
+			throws Exception {
+
+		Template tp = cfg.getTemplate(template);
+		StringWriter stringWriter = new StringWriter();
+		BufferedWriter writer = new BufferedWriter(stringWriter);
+		tp.setEncoding("UTF-8");
+		tp.process(content, writer);
+		String htmlStr = stringWriter.toString();
+		writer.flush();
+		writer.close();
+		return htmlStr;
+	}
+	@RequestMapping(value = "/printRecord/{uuid}.pdf",method=RequestMethod.GET)
+	public void ok(HttpServletResponse response,@PathVariable(name="uuid")  String uuid) {
+		
+		try {
+			FyTestRecord testRecord = testRecordMng.findByUuid(uuid);
+			 response.setHeader("content-disposition", "attachment;filename=" + URLEncoder.encode(uuid+".pdf", "UTF-8"));  
+			Map<String, Object> variables=new  HashMap<String, Object>();
+			
+
+			variables.put("title", "用户列表");
+			variables.put("testRecord", testRecord);
+
+			String htmlStr = generate(variables,"recordTemplate.html");
+
+			OutputStream out  = response.getOutputStream();//输出流  
+			PdfGenerator.generatePlus(htmlStr, out);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		
+	}
 	
 	@RequestMapping(value = "/submit",method=RequestMethod.POST)
 	public Result submit(String token,Long id,String answers) {
