4ee7b66d9d5e65f739c57bd196059df95123da67
chscls@163.com <chscls@163.com>
2018-06-12 14:16:50 +0800

    111111111111

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,8 +1,12 @@
 package com;
 
+import java.io.File;
+
 import javax.servlet.MultipartConfigElement;
 
 import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,6 +22,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -28,17 +33,16 @@ import com.zhitail.frame.util.jpa.BaseRepositoryFactoryBean;
 import com.zhitail.test.SnowflakeIdWorker;
 
 
-
-
+@EnableEurekaServer
+@EnableZuulProxy
+/*########################################*/
 @SpringBootApplication
 @ServletComponentScan
 @EnableEurekaClient
-/*@EnableZuulProxy
-@EnableFeignClients
-@EnableEurekaServer
-@EnableCircuitBreaker*/
 @EnableJpaRepositories(repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class)
 public class Application {
+	@Autowired
+	private Environment env;
 	private static long datacenterId = 0L;
 	private static long workerId = 0L;
 	private static Integer port=8080;
@@ -72,4 +76,33 @@ public class Application {
 	        factory.setMaxRequestSize("10000MB");  
 	        return factory.createMultipartConfig();  
 	    }  
+	 
+	 
+	 @Bean
+	    public EmbeddedServletContainerFactory servletContainer() {
+	        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+	        tomcat.addAdditionalTomcatConnectors(createSslConnector()); // 添加http
+	        return tomcat;
+	    }
+
+	 private Connector createSslConnector() {
+	        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+	        String x = env.getProperty("https.file");
+	        String y = env.getProperty("https.password");
+	        String p = env.getProperty("https.port");
+	            File keystore = new File(x);
+	            /*File truststore = new ClassPathResource("sample.jks").getFile();*/
+	            connector.setScheme("https");
+	            connector.setSecure(true);
+	            connector.setPort(Integer.parseInt(p));
+	            protocol.setSSLEnabled(true);
+	            protocol.setKeystoreFile(keystore.getAbsolutePath());
+	            protocol.setKeystorePass(y);
+	           // protocol.setKeyPass(key_password);
+	            return connector;
+	      
+	     
+	    }
+	 
 }
