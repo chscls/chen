e3824021b5f7048033983a95904bffa1cb03fd7a
chscls@163.com <chscls@163.com>
2018-06-15 13:51:37 +0800

    111111111

--- src/main/java/com/zhitail/app/config/CorsFilter.java
+++ src/main/java/com/zhitail/app/config/CorsFilter.java
@@ -14,7 +14,7 @@ public class CorsFilter implements Filter {
   
   
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-		System.out.println("Filtering on...........................................................");
+		
 		HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
