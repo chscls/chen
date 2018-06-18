a9be790082298997e9de3852aa74816bfc72483e
chscls@163.com <chscls@163.com>
2018-06-18 11:47:06 +0800

    1111111

--- src/main/java/com/zhitail/app/manager/Impl/FyTestMngImpl.java
+++ src/main/java/com/zhitail/app/manager/Impl/FyTestMngImpl.java
@@ -35,7 +35,7 @@ public class FyTestMngImpl implements FyTestMng {
 	private FyTestDao testDao;
 	@Autowired
 	private FyQuestionMng questionMng;
-	
+
 	public FyTest update(FyTest test) {
 		// TODO Auto-generated method stub
 		Updater u = new Updater(test);
@@ -61,19 +61,32 @@ public class FyTestMngImpl implements FyTestMng {
 	}
 
 	@Override
-	public FyTest updateTestQuestions(Long id, Long[] qids,Double score) {
-		FyTest  t= testDao.findOne(id);
+	public FyTest updateTestQuestions(Long id, Long[] qids, Double score) {
+		FyTest t = testDao.findOne(id);
+		List<QuestionConfig> list;
 		// TODO Auto-generated method stub
-		if(score==null) {
-			score=1.0;
+		if (score == null) {
+			score = 1.0;
+	
+		     list = new ArrayList<QuestionConfig>();
+		   for (Long qid : qids) {
+			  if (qid != null) {
+				QuestionConfig qc = new QuestionConfig(qid, score);
+				list.add(qc);
+			  }
+		   }
+		}else {
+			list = t.getQuestionConfigs();
+			 for (Long qid : qids) {
+				  if (qid != null) {
+					QuestionConfig qc = new QuestionConfig(qid, score);
+					int x  = list.indexOf(qc);
+					list.set(x, qc);
+				  }
+			 }
 		}
-		List<QuestionConfig> list = new ArrayList<QuestionConfig>();
-		for(Long qid:qids) {
-			if(qid!=null) {
-		QuestionConfig qc = new QuestionConfig(qid,score);
-		list.add(qc);
-			}
-		}
+		
+		
 		t.setJson(JSONArray.toJSONString(list));
 		t.refreshCode();
 		return update(t);
@@ -84,21 +97,22 @@ public class FyTestMngImpl implements FyTestMng {
 		// TODO Auto-generated method stub
 		Finder finder = Finder.create(" from FyTest bean where 1=1");
 
-		if (codes!= null) {
+		if (codes != null) {
 			finder.append(" and bean.code in:codes");
-			finder.setParamList("codes", codes );
+			finder.setParamList("codes", codes);
 		}
 
 		finder.append("and bean.isRecycle=false order by bean.id desc");
 		return testDao.findListByFinder(finder);
 	}
+
 	public void fullQuestions(FyTest test) {
 		FyQuestion temp;
 		List<QuestionConfig> ids = test.getQuestionConfigs();
 		if (ids.size() > 0) {
 			Long[] qids = new Long[ids.size()];
-			for(int i=0;i<ids.size();i++) {
-				qids[i]=ids.get(i).getId();
+			for (int i = 0; i < ids.size(); i++) {
+				qids[i] = ids.get(i).getId();
 			}
 			List<FyQuestion> qs = questionMng.findByIds(qids);
 			Map<Long, FyQuestion> map = new HashMap<Long, FyQuestion>(qs.size());
@@ -108,7 +122,7 @@ public class FyTestMngImpl implements FyTestMng {
 			List<FyQuestion> fqs = new ArrayList<FyQuestion>();
 			for (QuestionConfig config : ids) {
 				if (map.containsKey(config.getId())) {
-					temp= map.get(config.getId());
+					temp = map.get(config.getId());
 					temp.setScore(config.getScore());
 					fqs.add(temp);
 				}
@@ -120,6 +134,7 @@ public class FyTestMngImpl implements FyTestMng {
 		}
 
 	}
+
 	@Override
 	public List<FyTest> getList(Integer start, Integer count, FyTest search) {
 		// TODO Auto-generated method stub
@@ -131,12 +146,12 @@ public class FyTestMngImpl implements FyTestMng {
 		}
 		if (search.getUserId() != null) {
 			finder.append(" and bean.userId=:userId");
-			finder.setParam("userId",search.getUserId() );
+			finder.setParam("userId", search.getUserId());
 		}
-		
+
 		finder.setFirstResult(start);
 		finder.setMaxResults(count);
-		
+
 		finder.append("and bean.isRecycle=false order by bean.id desc");
 		return testDao.findListByFinder(finder);
 	}
@@ -160,8 +175,8 @@ public class FyTestMngImpl implements FyTestMng {
 	}
 
 	@Override
-	public Pagination<FyTest> getPage(Long userId, Integer pageNo, Integer pageSize,String code, String title, String isQuestionnaire,String isNoOrder,
-			String mode,String status,String sorter) {
+	public Pagination<FyTest> getPage(Long userId, Integer pageNo, Integer pageSize, String code, String title,
+			String isQuestionnaire, String isNoOrder, String mode, String status, String sorter) {
 		// TODO Auto-generated method stub
 		Finder finder = Finder.create(" from FyTest bean where 1=1");
 
@@ -191,7 +206,7 @@ public class FyTestMngImpl implements FyTestMng {
 			finder.append(" and bean.status in:statuss");
 			finder.setParamList("statuss", tt.toArray(new Status[tt.size()]));
 		}
-		
+
 		if (StringUtils.isNotBlank(isQuestionnaire)) {
 			String[] t = isQuestionnaire.split(",");
 			List<Boolean> tt = new ArrayList<Boolean>();
@@ -213,9 +228,9 @@ public class FyTestMngImpl implements FyTestMng {
 		finder.append(" and bean.userId=:userId");
 		finder.setParam("userId", userId);
 		finder.append(" and bean.isRecycle=false ");
-		if(StringUtils.isNotBlank(sorter)&&sorter.equals("createTime_ascend")){
+		if (StringUtils.isNotBlank(sorter) && sorter.equals("createTime_ascend")) {
 			finder.append(" order by bean.createTime asc");
-		}else {
+		} else {
 			finder.append(" order by bean.createTime desc");
 		}
 		return testDao.findPageByFinder(finder, pageNo, pageSize);
@@ -226,51 +241,51 @@ public class FyTestMngImpl implements FyTestMng {
 		// TODO Auto-generated method stub
 		FyTest test;
 		for (Long id : ids) {
-			test=this.findById(id);
-			if(test!=null) {
-			test.setRecycleTime(new Date());
-			test.setIsRecycle(true);
-			this.update(test);
+			test = this.findById(id);
+			if (test != null) {
+				test.setRecycleTime(new Date());
+				test.setIsRecycle(true);
+				this.update(test);
 			}
 		}
 	}
 
 	@Override
-	public Pagination<FyTest> getRecyclePage(Long userId, Integer pageNo, Integer pageSize,String title,String code,String sorter) {
+	public Pagination<FyTest> getRecyclePage(Long userId, Integer pageNo, Integer pageSize, String title, String code,
+			String sorter) {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
-				Finder finder = Finder.create(" from FyTest bean where 1=1");
-				if (StringUtils.isNotBlank(title)) {
-					finder.append(" and bean.title like:title");
-					finder.setParam("title", "%" + title + "%");
-				}
-				if (StringUtils.isNotBlank(code)) {
-					finder.append(" and bean.code like:code");
-					finder.setParam("code", "%" + code + "%");
-				}
-				finder.append(" and bean.userId=:userId");
-				finder.setParam("userId", userId);
-				finder.append(" and bean.isRecycle=true ");
-				if(StringUtils.isNotBlank(sorter)&&sorter.equals("recycleTime_ascend")){
-					finder.append(" order by bean.recycleTime asc");
-				}else {
-					finder.append(" order by bean.recycleTime desc");
-				}
-				
-				
-				return testDao.findPageByFinder(finder, pageNo, pageSize);
-			}
+		Finder finder = Finder.create(" from FyTest bean where 1=1");
+		if (StringUtils.isNotBlank(title)) {
+			finder.append(" and bean.title like:title");
+			finder.setParam("title", "%" + title + "%");
+		}
+		if (StringUtils.isNotBlank(code)) {
+			finder.append(" and bean.code like:code");
+			finder.setParam("code", "%" + code + "%");
+		}
+		finder.append(" and bean.userId=:userId");
+		finder.setParam("userId", userId);
+		finder.append(" and bean.isRecycle=true ");
+		if (StringUtils.isNotBlank(sorter) && sorter.equals("recycleTime_ascend")) {
+			finder.append(" order by bean.recycleTime asc");
+		} else {
+			finder.append(" order by bean.recycleTime desc");
+		}
+
+		return testDao.findPageByFinder(finder, pageNo, pageSize);
+	}
 
 	@Override
 	public void recovery(Long[] ids) {
 		// TODO Auto-generated method stub
 		FyTest test;
 		for (Long id : ids) {
-			test=this.findById(id);
-			if(test!=null) {
-			test.setRecycleTime(null);
-			test.setIsRecycle(false);
-			this.update(test);
+			test = this.findById(id);
+			if (test != null) {
+				test.setRecycleTime(null);
+				test.setIsRecycle(false);
+				this.update(test);
 			}
 		}
 	}
@@ -279,39 +294,40 @@ public class FyTestMngImpl implements FyTestMng {
 	public void clear() {
 		// TODO Auto-generated method stub
 		Finder finder = Finder.create("select bean.id from FyTest bean where 1=1");
-		
-			finder.append(" and bean.recycleTime <:overTime");
-			Date now = new Date(new Date().getTime()-1000*60*60*24*15);
-			finder.setParam("overTime",now);
-			finder.append(" and bean.isRecycle=true");
-			List<Object> list = 	testDao.findObjectListByFinder(finder);
-			Long[] x = list.toArray(new Long[list.size()]);
-			this.delete(x);
+
+		finder.append(" and bean.recycleTime <:overTime");
+		Date now = new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 15);
+		finder.setParam("overTime", now);
+		finder.append(" and bean.isRecycle=true");
+		List<Object> list = testDao.findObjectListByFinder(finder);
+		Long[] x = list.toArray(new Long[list.size()]);
+		this.delete(x);
 	}
 
 	@Override
 	public void upShopTest(Long[] ids) {
 		FyTest test;
 		for (Long id : ids) {
-			test=this.findById(id);
-			if(test!=null&&(test.getSaleStatus()==SaleStatus.create||test.getSaleStatus()==SaleStatus.refuse)) {
-			test.setSaleStatus(SaleStatus.apply);
-			this.update(test);
+			test = this.findById(id);
+			if (test != null
+					&& (test.getSaleStatus() == SaleStatus.create || test.getSaleStatus() == SaleStatus.refuse)) {
+				test.setSaleStatus(SaleStatus.apply);
+				this.update(test);
 			}
 		}
-		
+
 	}
 
 	@Override
 	public void downShopTest(Long[] ids) {
 		FyTest test;
 		for (Long id : ids) {
-			test=this.findById(id);
-			if(test!=null&&test.getSaleStatus()==SaleStatus.sale) {
+			test = this.findById(id);
+			if (test != null && test.getSaleStatus() == SaleStatus.sale) {
 				test.setSaleStatus(SaleStatus.create);
-			this.update(test);
+				this.update(test);
 			}
 		}
-		
+
 	}
 }
