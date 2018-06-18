17e95855c85404f4f67eab932b1d5b065cb3c2f6
chscls@163.com <chscls@163.com>
2018-06-18 21:19:10 +0800

    1111

--- src/main/java/com/zhitail/app/entity/middle/FyTestRecordStatistics.java
+++ src/main/java/com/zhitail/app/entity/middle/FyTestRecordStatistics.java
@@ -1,9 +1,24 @@
 package com.zhitail.app.entity.middle;
 
 import com.zhitail.app.entity.FyTest;
+import com.zhitail.app.entity.FyTest.Mode;
 
 public class FyTestRecordStatistics {
 		private String code;
+		private String title;
+		private Mode mode;
+		public Mode getMode() {
+			return mode;
+		}
+		public void setMode(Mode mode) {
+			this.mode = mode;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
 		private Long count;
 		private Double maxScore;
 		private Double minScore;
@@ -21,12 +36,14 @@ public class FyTestRecordStatistics {
 			this.count = count;
 		}
 		private FyTest test;
-		public FyTestRecordStatistics (String code,Long count,Double maxScore,Double minScore,Double avgScore){
+		public FyTestRecordStatistics (String code,String title,Long count,Double maxScore,Double minScore,Double avgScore,Mode mode){
 			this.code=code;
 			this.count=count;
 			this.maxScore=maxScore;
 			this.minScore=minScore;
 			this.avgScore =avgScore;
+			this.title=title;
+			this.mode=mode;
 		}
 		public Double getMinScore() {
 			return minScore;
