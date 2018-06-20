e459ef233bf7fccc7d8f8dcdb913ba4b000ffb89
chscls@163.com <chscls@163.com>
2018-06-20 11:32:49 +0800

    1111111

--- src/main/java/com/zhitail/app/entity/FyFriend.java
+++ src/main/java/com/zhitail/app/entity/FyFriend.java
@@ -1,5 +1,6 @@
 package com.zhitail.app.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,8 +8,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 @Entity
-@Table(name = "fy_friend")
+@Table(name = "fy_friend",uniqueConstraints = {
+	      @UniqueConstraint(columnNames = {"user_id", "friend_id"})
+	})
 public class FyFriend {
 	public Long getId() {
 		return id;
@@ -43,8 +47,10 @@ public class FyFriend {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@Column(name = "user_id")
 	private Long userId;
 	private String realname;
+	@Column(name = "friend_id")
 	private Long friendId;
 	@ManyToOne
 	@JoinColumn(name = "group_id")
