1739fdb53f3c1ab9ea6a9fd240f5bc6df584325c
chscls@163.com <chscls@163.com>
2018-07-05 13:04:29 +0800

    11111111

--- src/main/java/com/zhitail/app/entity/FyUserSkin.java
+++ src/main/java/com/zhitail/app/entity/FyUserSkin.java
@@ -20,7 +20,7 @@ public class FyUserSkin {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
-	private String userId;
+	private Long userId;
 	@ManyToOne
 	@JoinColumn(name = "skin_id")
 	private FySkin skin;
@@ -53,13 +53,13 @@ public class FyUserSkin {
 	public void setBuyTime(Date buyTime) {
 		this.buyTime = buyTime;
 	}
-	public String getUserId() {
+	public Long getUserId() {
 		return userId;
 	}
-	public void setUserId(String userId) {
+	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
-
+	
 	
 
 }
