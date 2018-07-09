cf33629a51bceaee61f0aee5c382d21465bd937f
chscls@163.com <chscls@163.com>
2018-07-09 14:42:14 +0800

    111111111111

--- src/main/java/com/zhitail/app/entity/FyCatalog.java
+++ src/main/java/com/zhitail/app/entity/FyCatalog.java
@@ -124,7 +124,7 @@ public class FyCatalog implements ShowAbleInterface{
 		sa.setImg(img);
 		sa.setPath(path);
 		sa.setTemplete(templete);
-		
+		sa.setName(name);
 		return sa;
 	}
 
--- src/main/java/com/zhitail/app/entity/middle/ShowAble.java
+++ src/main/java/com/zhitail/app/entity/middle/ShowAble.java
@@ -2,6 +2,13 @@ package com.zhitail.app.entity.middle;
 
 public class ShowAble {
 	private Long id;
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	public Long getId() {
 		return id;
 	}
